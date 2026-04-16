81346aee04d72c8c96709eff1cbdb529d0457734
Yasmeen saleh <lap top@DESKTOP-40R4L6L>
2026-04-16 11:54:05 +0300

    Finish Function Part2

---
+++ Function Part 2/Program.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Function_Part2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1) Create a function that takes two dates and returns the number of days between the first
+            ////and second date.
+            //Console.WriteLine("----------Task 1-----------");
+
+            //Console.WriteLine("Enter The First Date Example (June 14, 2019)");
+            //DateTime firstDate = Convert.ToDateTime(Console.ReadLine());
+            //Console.WriteLine("Enter The Second Date");
+            //DateTime SecondDate = Convert.ToDateTime(Console.ReadLine());
+            //static int getDaysBetween(DateTime firstDate , DateTime SecondDate)
+            //{
+            //    TimeSpan difference = SecondDate - firstDate;
+            //    return Math.Abs(difference.Days);
+
+            //}
+            //Console.WriteLine($"The Difference between {firstDate.ToString()} and {SecondDate.ToString()} is {getDaysBetween(firstDate, SecondDate)}");
+            //// 2) Create a function that takes an array of strings and returns an array with only the strings
+            ////that have numbers in them.If there are no strings containing numbers, return an empty
+            ////array.
+            //Console.WriteLine("----------Task 2-----------");
+
+            //static List<string> numInStr(string[] strings)
+            //{
+            //    List<string> filteredList = new List<string>();
+            //  filteredList=  strings.Where(x => x.Any(char.IsDigit)).ToList();
+            //    return filteredList;
+
+            //}
+            //Console.WriteLine($"The Filterd List is [{string.Join(",", numInStr(["1a", "a", "2b", "b"]))}]");
+            ////3) Given a string, reverse all the words which have odd length. The even length words are 
+            ////not changed. 
+            ////Console.WriteLine("----------Task 3-----------");
+
+            //Console.WriteLine("Enter your words ");
+            //string input = Console.ReadLine();
+            //static string reverseOdd(string input)
+            //{
+            //    string[] words = input.Split(' ');
+
+            //    for (int i = 0; i < words.Length; i++)
+            //    {
+            //        if (words[i].Length % 2 != 0)
+            //        {
+            //            char[] charArray = words[i].ToCharArray();
+            //            Array.Reverse(charArray);
+            //            words[i] = new string(charArray);
+            //        }
+            //    }
+            //    return string.Join(" ", words);
+            //}
+
+            //Console.WriteLine(reverseOdd(input));
+
+            //4) A pandigital number contains all digits(0 - 9) at least once.Write a function that takes an
+            //integer, returning true if the integer is pandigital, and false otherwise
+            Console.WriteLine("----------Task 4-----------");
+            Console.WriteLine("enter your number");
+            double num = double.Parse(Console.ReadLine());    
+            static bool isPandigital(double num)
+            {
+                string numStr = num.ToString();
+                for(int i = 0; i <= 9; i++)
+                {
+                    if (!numStr.Contains(i.ToString()))
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            Console.WriteLine(isPandigital(num));
+
+
+
+        }
+    }
+}
--- Function Part1/Program.cs
+++ Function Part1/Program.cs
@@ -12,149 +12,150 @@ namespace Function_Part1
     {
         static void Main(string[] args)
         {
-        //    //1) Write a function that takes an integer minutes and converts it to seconds.
-        //    Console.WriteLine("----------Task 1-----------");
+            //1) Write a function that takes an integer minutes and converts it to seconds.
+            Console.WriteLine("----------Task 1-----------");
 
-        //    Console.WriteLine("Write muints");
-        //    int minutes = int.Parse(Console.ReadLine());
-        //    static int convertMinutesToSeconds(int minutes)
-        //    {
-        //        int seconds = minutes * 60;
-        //        return seconds;
+            Console.WriteLine("Write muints");
+            int minutes = int.Parse(Console.ReadLine());
+            static int convertMinutesToSeconds(int minutes)
+            {
+                int seconds = minutes * 60;
+                return seconds;
 
-        //    }
-        //    Console.WriteLine($" {minutes} m = {convertMinutesToSeconds(minutes)} s");
-        //    //2) Create a function that takes a number as an argument, increments the number by +1 and
-        //    //returns the result.
-        //    Console.WriteLine("----------Task 2-----------");
+            }
+            Console.WriteLine($" {minutes} m = {convertMinutesToSeconds(minutes)} s");
+            //2) Create a function that takes a number as an argument, increments the number by +1 and
+            //returns the result.
+            Console.WriteLine("----------Task 2-----------");
 
-        //    Console.WriteLine("Write Number");
-        //    int value = int.Parse(Console.ReadLine());
-        //    static int incrementNumber(int value)
-        //    {
-        //       return  value = value + 1;
-        //    }
-        //    Console.WriteLine($"The increment value of {value} is {incrementNumber(value)}");
-        //    //3) Create a function that takes an array containing only numbers and return the first element.
-        //    Console.WriteLine("----------Task 3-----------");
+            Console.WriteLine("Write Number");
+            int value = int.Parse(Console.ReadLine());
+            static int incrementNumber(int value)
+            {
+                return value = value + 1;
+            }
+            Console.WriteLine($"The increment value of {value} is {incrementNumber(value)}");
+            //3) Create a function that takes an array containing only numbers and return the first element.
+            Console.WriteLine("----------Task 3-----------");
 
-        //    List<int> numbers = new List<int>();
-        //    Console.WriteLine("How many numbers do you want to add?");
-        //    int count = int.Parse(Console.ReadLine());
+            List<int> numbers = new List<int>();
+            Console.WriteLine("How many numbers do you want to add?");
+            int count = int.Parse(Console.ReadLine());
 
-        //    for (int i = 0; i < count; i++)
-        //    {
-        //        Console.Write($"Enter number {i + 1}: ");
-        //        numbers.Add(int.Parse(Console.ReadLine()));
-        //    }
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write($"Enter number {i + 1}: ");
+                numbers.Add(int.Parse(Console.ReadLine()));
+            }
 
-        //    if (numbers.Count > 0)
-        //    {
-        //        int firstElement = GetFirstElement(numbers);
-        //        Console.WriteLine($"The first element is: {firstElement}");
-        //    }
-        //    else
-        //    {
-        //        Console.WriteLine("The list is empty!");
-        //    }static int GetFirstElement(List<int> list)
-        //{
-        //    return list[0];
-    
-        //}
-        //    //4) Write a function that takes the base and height of a triangle and return its area
-        //    Console.WriteLine("----------Task 4-----------");
+            if (numbers.Count > 0)
+            {
+                int firstElement = GetFirstElement(numbers);
+                Console.WriteLine($"The first element is: {firstElement}");
+            }
+            else
+            {
+                Console.WriteLine("The list is empty!");
+            }
+            static int GetFirstElement(List<int> list)
+            {
+                return list[0];
 
-        //    Console.WriteLine("Enter Height");
-        //    double  height = double.Parse(Console.ReadLine());
-        //    Console.WriteLine("Enter The Base");
-        //    double triangleBase = double.Parse(Console.ReadLine());
-        //    static double calculateTheArea(double height , double triangleBase)
-        //    {
-                
-        //        return ( height * triangleBase) / 2;
+            }
+            //4) Write a function that takes the base and height of a triangle and return its area
+            Console.WriteLine("----------Task 4-----------");
+
+            Console.WriteLine("Enter Height");
+            double height = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter The Base");
+            double triangleBase = double.Parse(Console.ReadLine());
+            static double calculateTheArea(double height, double triangleBase)
+            {
+
+                return (height * triangleBase) / 2;
 
 
 
-        //    }
-        //    Console.WriteLine($"The Area of triangle with height {height} and Base {triangleBase} is  {calculateTheArea(height, triangleBase)}");
-        //    //5) Create a function called evenNumberEvenIndex
-        //    // that accept an array of nums
-        //    // and return a new array that have the even number in even index
-        //    //var nums = [5, 2, 2, 1, 8, 66, 55, 77, 34, 9, 55, 1]
-        //    //Ex: evenNumberEvenIndex (nums) => [2, 8, 34]
-        //    Console.WriteLine("----------Task 5-----------");
+            }
+            Console.WriteLine($"The Area of triangle with height {height} and Base {triangleBase} is  {calculateTheArea(height, triangleBase)}");
+            //5) Create a function called evenNumberEvenIndex
+            // that accept an array of nums
+            // and return a new array that have the even number in even index
+            //var nums = [5, 2, 2, 1, 8, 66, 55, 77, 34, 9, 55, 1]
+            //Ex: evenNumberEvenIndex (nums) => [2, 8, 34]
+            Console.WriteLine("----------Task 5-----------");
 
-        //    static List<int> evenNumberEvenIndex(int[] ints)
-        //    {
-        //        List<int> evenNumberEvenIndexArr = new List<int>();
+            static List<int> evenNumberEvenIndex(int[] ints)
+            {
+                List<int> evenNumberEvenIndexArr = new List<int>();
 
-        //        for (int i = 0;i < ints.Length;i++)
-        //        {
-        //            if(ints[i] % 2 == 0 && i% 2 == 0)
-        //            {
+                for (int i = 0; i < ints.Length; i++)
+                {
+                    if (ints[i] % 2 == 0 && i % 2 == 0)
+                    {
 
-        //                evenNumberEvenIndexArr.Add(ints[i]);
+                        evenNumberEvenIndexArr.Add(ints[i]);
 
-        //            }
-        //        }
-        //        return evenNumberEvenIndexArr;
-        //    }
-        //    var result = evenNumberEvenIndex(([5, 2, 2, 1, 8, 66, 55, 77, 34, 9, 55, 1]));
-        //    Console.WriteLine("[" + string.Join(", ", result) + "]");
-        //    //6) Create a function called evenIndexOddLength that accept an array of strings
-        //    // and return a new array that have the string with odd length in even index
-        //    //var strings = ["alex", "mercer", "madrasa", "rashed2", "emad", "hala"]
-        //    //Ex: evenIndexOddLength (strings) => ["madrasa"]
-        //    Console.WriteLine("----------Task 6-----------");
+                    }
+                }
+                return evenNumberEvenIndexArr;
+            }
+            var result = evenNumberEvenIndex(([5, 2, 2, 1, 8, 66, 55, 77, 34, 9, 55, 1]));
+            Console.WriteLine("[" + string.Join(", ", result) + "]");
+            //6) Create a function called evenIndexOddLength that accept an array of strings
+            // and return a new array that have the string with odd length in even index
+            //var strings = ["alex", "mercer", "madrasa", "rashed2", "emad", "hala"]
+            //Ex: evenIndexOddLength (strings) => ["madrasa"]
+            Console.WriteLine("----------Task 6-----------");
 
-        //    static List<string> evenIndexOddLength(string[] names)
-        //    {
-        //         List<string> evenIndexOddLengthArr = new List<string>();
-        //        for(int i =0; i< names.Length;i++)
-        //        {
-        //            if(names[i].Length % 2 != 0 && i % 2 == 0)
-        //            {
-        //                evenIndexOddLengthArr.Add(names[i]);
-        //            }
-        //        }
-        //        return evenIndexOddLengthArr;
-        //    }
-        //    var arr = evenIndexOddLength(["alex", "mercer", "madrasa", "rashed2", "emad", "hala"]);
-        //    Console.Write(string.Join(", ", arr));
-        //    // 7) Create a function called powerElementIndex that accept an array of number
-        //    //and return a new array that have the element power by the index of it self
-        //    //var nums = [44, 5, 4, 3, 2, 10]
-        //    //Ex: powerElementIndex (nums) => [0, 5, 16, 27, 16, 100000]
-        //    Console.WriteLine("\n----------Task 7-----------");
-        //    static List<double>  powerElementIndex(double [] numbers)
-        //    {
-        //        List<double> powerArr = new List<double>();
-        //        for (int i = 0; i< numbers.Length; i++)
-        //        {
-        //          double power=  Math.Pow(numbers[i], i);
-        //            powerArr.Add(power);
-        //        }
-        //        return powerArr;
+            static List<string> evenIndexOddLength(string[] names)
+            {
+                List<string> evenIndexOddLengthArr = new List<string>();
+                for (int i = 0; i < names.Length; i++)
+                {
+                    if (names[i].Length % 2 != 0 && i % 2 == 0)
+                    {
+                        evenIndexOddLengthArr.Add(names[i]);
+                    }
+                }
+                return evenIndexOddLengthArr;
+            }
+            var arr = evenIndexOddLength(["alex", "mercer", "madrasa", "rashed2", "emad", "hala"]);
+            Console.Write(string.Join(", ", arr));
+            // 7) Create a function called powerElementIndex that accept an array of number
+            //and return a new array that have the element power by the index of it self
+            //var nums = [44, 5, 4, 3, 2, 10]
+            //Ex: powerElementIndex (nums) => [0, 5, 16, 27, 16, 100000]
+            Console.WriteLine("\n----------Task 7-----------");
+            static List<double> powerElementIndex(double[] numbers)
+            {
+                List<double> powerArr = new List<double>();
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    double power = Math.Pow(numbers[i], i);
+                    powerArr.Add(power);
+                }
+                return powerArr;
 
-        //    }
-        //    Console.WriteLine(string.Join(",", powerElementIndex([44, 5, 4, 3, 2, 10])));
-        //    //8) Write a function called multiplication2
-        //    //that takes two parameters
-        //    //and return the multiplication of them
-        //    //you need to use only sum
-        //    //multiplication2(5, 4) => 20
-        //    //multiplication2(2, 8) => 16
-        //    //multiplication2(7, 6) => 42
-        //    Console.WriteLine("----------Task 8 -----------");
-        //    Console.WriteLine("Enter the First num :");
-        //    double firstNum = double.Parse(Console.ReadLine());
-        //    Console.WriteLine("Enter the Second num :");
-        //    double secondNum = double.Parse(Console.ReadLine());
-        //    static double  multiplication2(double firstNum , double secondNum)
-        //    {
-        //        return firstNum * secondNum;
+            }
+            Console.WriteLine(string.Join(",", powerElementIndex([44, 5, 4, 3, 2, 10])));
+            //8) Write a function called multiplication2
+            //that takes two parameters
+            //and return the multiplication of them
+            //you need to use only sum
+            //multiplication2(5, 4) => 20
+            //multiplication2(2, 8) => 16
+            //multiplication2(7, 6) => 42
+            Console.WriteLine("----------Task 8 -----------");
+            Console.WriteLine("Enter the First num :");
+            double firstNum = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Second num :");
+            double secondNum = double.Parse(Console.ReadLine());
+            static double multiplication2(double firstNum, double secondNum)
+            {
+                return firstNum * secondNum;
 
-        //    }
+            }
             //Console.WriteLine($"The multiplication of {firstNum} and {secondNum} = {multiplication2(firstNum,secondNum)} ");
             //9) Create a function called muti2
             //  that take two parameter
